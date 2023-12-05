ccc8551d39868e6bf7e1e2ec9ad68a5392c73136
UIT21522155 <21522155@gm.uit.edu.vn>
2023-12-05 20:06:39 +0700

    HomePage ver_1

---
+++ Pages/HomePage/header.php
@@ -0,0 +1,15 @@
+<div class="topbar">
+            <div class="prev-next-buttons">
+                <button type="button" class="fa fas fa-chevron-left"></button>
+                <button type="button" class="fa fas fa-chevron-right"></button>
+            </div>
+
+            <div class="navbar">
+                <ul>
+                    <li>
+                        <a href="">Sign Up</a>
+                    </li>
+                </ul>
+                <button type="button">Login</button>
+            </div>
+        </div>
