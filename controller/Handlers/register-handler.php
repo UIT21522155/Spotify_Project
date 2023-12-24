de7410d8ba51b4c59e7766dc290fb1f103e37de4
UIT-21522042 <96830385+hang21522042@users.noreply.github.com>
2023-12-24 22:39:37 +0700

    24/12 php login register
     php login register

---
+++ controller/Handlers/register-handler.php
@@ -0,0 +1,36 @@
+<?php 
+function sanitizeFormUserName($stringInput){
+	$stringInput = strip_tags($stringInput);
+	$stringInput = str_replace(" ", "", $stringInput);
+	return $stringInput;
+}
+
+function sanitizeFormString($stringInput){
+	$stringInput = strip_tags($stringInput);
+	$stringInput = str_replace(" ", "", $stringInput);
+	$stringInput=ucfirst(strtolower($stringInput));
+	return $stringInput;
+}
+
+function sanitizeFormPassword($stringInput)
+{
+	$stringInput = strip_tags($stringInput);
+	return $stringInput;
+}
+
+if (isset($_POST["registerButton"])) {
+	$username = sanitizeFormUserName($_POST["username"]);
+	$name = sanitizeFormString($_POST["name"]);
+	$email = sanitizeFormString($_POST["email"]);
+	$email2 = sanitizeFormString($_POST["email2"]);
+	$password = sanitizeFormPassword($_POST["password"]);
+	$password2 = sanitizeFormPassword($_POST["password2"]);
+
+	$wasSuccessful = $account->register($username,$name,$email,$email2,$password,$password2);
+	if($wasSuccessful){
+		$_SESSION['userLoggedIn'] = $username;
+		header("Location:index.php");
+	}
+}
+
+ ?>
